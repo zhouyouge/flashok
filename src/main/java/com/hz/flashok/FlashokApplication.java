5dcf97aa5d02512bd01fda371c571e21e201fad3
majie <majie>
2019-09-06 09:23:11 +0800

    nmajie

--- src/main/java/com/hz/flashok/FlashokApplication.java
+++ src/main/java/com/hz/flashok/FlashokApplication.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.ComponentScan;
 @ComponentScan(basePackages = {"com.hz.service","com.hz.controller"})
 @MapperScan("com.hz.dao")
 public class FlashokApplication {
+    //xxxx
     public static void main(String[] args) {
         SpringApplication.run(FlashokApplication.class, args);
     }
