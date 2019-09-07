b93481eaee2777160a1c50522dc6f55a87070663
Dell <asdasd>
2019-09-07 11:02:48 +0800

    lwh

--- src/main/java/com/hz/service/impl/AdminLoginServiceImPL.java
+++ src/main/java/com/hz/service/impl/AdminLoginServiceImPL.java
@@ -2,20 +2,22 @@ package com.hz.service.impl;
 
 import com.hz.dao.AdminLoginMapper;
 import com.hz.pojo.FoAdmin;
+import com.hz.service.AdminLoginService;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 
-@Service
-public class AdminLoginServiceImPL {
+@Service("adminLoginService")
+public class AdminLoginServiceImPL implements AdminLoginService {
     @Resource
     private AdminLoginMapper adminLoginMapper;
+    @Override
     public FoAdmin adminLogin() throws Exception {
         return adminLoginMapper.adminLogin();
     }
-
+    @Override
     public int enrollAdmin(FoAdmin foAdmin)throws Exception{
         return adminLoginMapper.enrollAdmin(foAdmin);
-    };
+    }
 
 }
